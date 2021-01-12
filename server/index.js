449da02f2c415485f59ac84514699ff0eb4d761f
nightwarriorftw <amanv1999@gmail.com>
2021-01-12 13:49:43 +0530

    Fixed bug in posts request

--- server/index.js
+++ server/index.js
@@ -1,16 +1,15 @@
 import express from "express";
-import mongoose from "mongoose";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
 import cors from "cors";
 
 import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors);
+app.use(cors());
 
 app.use("/posts", postRoutes);
 
@@ -21,8 +20,10 @@ const PORT = process.env.PORT || 5000;
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
-    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
   )
-  .catch((err) => console.log(err));
+  .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set("useFindAndModify", false);
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -4,8 +4,7 @@ import { getPosts, createPost } from "../controllers/posts.js";
 
 const router = express.Router();
 
-
-router.get('/', getPosts);
-router.post('/', createPost);
+router.get("/", getPosts);
+router.post("/", createPost);
 
 export default router;
